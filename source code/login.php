765ead44e0071ae45dea44c0a711f58e9e815b91
Student <Student@tdc.edu.vn>
2021-03-29 09:05:14 +0700

    dftgrf

---
+++ source code/login.php
@@ -0,0 +1,26 @@
+<?php
+// LOGIN.PHP
+// HỒ SĨ HÙNG - LÊ TUẤN LIÊM
+session_start();
+require 'loginModel.php';
+$loginModel = new loginModel();
+
+if (!empty($_POST['user']) && !empty($_POST['pass'])) {
+    if ($loginModel->logIn($_POST['user'], $_POST['pass']) == 1) {
+        $_SESSION["account"] = $_POST['user'];
+
+        //Lấy id để phục vụ shopping cart
+        $_SESSION["ID_account"] = $loginModel->getIDByUserName($_POST['user'])["ID_account"];
+        header("Location: loginSuccessfully.html");
+    }
+    else if ($loginModel->logIn($_POST['user'], $_POST['pass']) == 0) {
+        $_SESSION["admin"] = $_POST['user'];
+
+        //Lấy id để phục vụ shopping cart
+        $_SESSION["ID_account"] = $loginModel->getIDByUserName($_POST['user'])["ID_account"];
+        header("Location: loginSuccessfully.html");
+    }
+?>
+<?php
+}
+?>
